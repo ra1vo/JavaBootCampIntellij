c5fefe6c7d16435a765dafec2ba5f4b5274090a8
raivo <raivo.dzelzkalns@gmail.com>
2021-03-02 11:19:09 +0200

    num to words activity

--- SEF - Participants Workspace/SEF/src/sef/module3/activity/NumToWords.java
+++ SEF - Participants Workspace/SEF/src/sef/module3/activity/NumToWords.java
@@ -1,45 +1,57 @@
 /**
- * 
+ *
  */
-package sef.module3.activity;
+package activity;
 
 /**
- * @author 
+ * @author
  *
  */
 public class NumToWords {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
-		
-				
-		int i = 8;
-		String numText = "";
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
 
-		switch(i) {
-		
-		case 1:
-			numText = "ONE";break;
-		case 2:
-			numText = "TWO";break;
-		case 3:
-			numText = "THREE";break;
-		case 4:
-			numText = "FOUR";break;
-		case 5:
-			numText = "FIVE";break;
-		// complete the code.
-		default:
-			numText = "NUMBER " + i;
-		}
-		
-		System.out.println(numText);
-		
-	
-	}
-	
+
+        int i = 5;
+        String numText = "";
+
+        switch (i) {
+
+            case 1:
+                numText = "ONE";
+                break;
+            case 2:
+                numText = "TWO";
+                break;
+            case 3:
+                numText = "THREE";
+                break;
+            case 4:
+                numText = "FOUR";
+                break;
+            case 5:
+                numText = "FIVE";
+                break;
+            case 6:
+                numText = "SIX";
+                break;
+            case 7:
+                numText = "SEVEN";
+                break;
+            case 8:
+                numText = "EIGHT";
+                break;
+            default:
+                numText = "NUMBER " + i;
+        }
+
+        System.out.println(numText);
+
+
+    }
+
 
 }
