1b9d3240d5b33c116311a739a6919562b06e1a11
raivo <raivo.dzelzkalns@gmail.com>
2021-03-02 15:28:14 +0200

    date activity

---
+++ SEF - Participants Workspace/SEF/src/sef/module4/activity/DateActivity2.java
@@ -0,0 +1,11 @@
+package activity;
+
+import java.util.Date;
+
+public class DateActivity2 {
+
+    public static void main(String[] args) {
+        Date today = new Date();
+        System.out.println(today);
+    }
+}
