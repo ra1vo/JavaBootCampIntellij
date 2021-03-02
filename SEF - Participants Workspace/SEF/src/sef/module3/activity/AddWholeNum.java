60917596c18f998d0ab790284a629dc01f99f0b1
raivo <raivo.dzelzkalns@gmail.com>
2021-03-02 11:24:08 +0200

    add whole num activity

--- SEF - Participants Workspace/SEF/src/sef/module3/activity/AddWholeNum.java
+++ SEF - Participants Workspace/SEF/src/sef/module3/activity/AddWholeNum.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package sef.module3.activity;
+package activity;
 
 /**
  * @author 
@@ -20,12 +20,14 @@ public class AddWholeNum {
 		System.out.println("Addition of Whole Numbers from 1 to 50 = " + total);
 		
 		total = 0;
-		
+
 		// complete the code. write for loop for i
-		total = total + i;
+
+		
+		for (int i = 50; i <= 100; i++) {
+			total = total + i;
 		}
 		System.out.println("Addition of Whole Numbers from 50 to 100 = " + total);
-
 
 	}
 
