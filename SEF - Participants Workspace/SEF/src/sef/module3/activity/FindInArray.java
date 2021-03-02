9eacdb7041b1331442ffaeba704d9ce726d500de
raivo <raivo.dzelzkalns@gmail.com>
2021-03-02 17:06:21 +0200

    find in array activity

--- SEF - Participants Workspace/SEF/src/sef/module3/activity/FindInArray.java
+++ SEF - Participants Workspace/SEF/src/sef/module3/activity/FindInArray.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package sef.module3.activity;
+package activity;
 
 /**
  * @author 
@@ -32,6 +32,8 @@ public class FindInArray {
 		// Find largest in an array
 		for (int i = 1; i < nums.length; i++) {
 			// complete the code
+			if (nums[i] > result)
+				result = nums[i];
 		}
 		System.out.println("Largest in array is "+result);
 
